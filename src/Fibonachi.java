090e8b564b3a5f41498b5fa14b613c2f71d8e6e8
Admin <olexandr.11.f@gmail.com>
2024-09-25 11:25:16 +0300

    Add JavaDoc

--- src/Fibonachi.java
+++ src/Fibonachi.java
@@ -3,22 +3,38 @@ public class Fibonachi {
     private int N;
     private int fib;
 
+    /**
+     *Function to create the class
+     */
     Fibonachi(int N, int fib) {
         this.N = N;
         this.fib = fib;
     }
 
+    /**
+     *Returns the value of N
+     */
     public int getN() {
         return N;
     }
 
+    /**
+     *Updates the value of N
+     */
     public void setN(int n) {
         N = n;
     }
 
+    /**
+     *Returns the value of the Fibonacci number
+     */
     public int getFib() {
         return fib;
     }
+
+    /**
+     *Updates the value of the Fibonacci number
+     */
     public void setFib(int fib) {
         this.fib = fib;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,47 @@
 import java.util.Scanner;
 
 class Main {
-    static int fib_sum = 1;
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter N: ");
         int N = sc.nextInt();
 
-        Fibonachi fib = new Fibonachi(N, fibonachi_SQ(N));
+        Fibonachi fib = new Fibonachi(N, fibonachi(N));
 
         System.out.println("The " + fib.getN() + "'s number is: " + fib.getFib());
-        System.out.println("The sum of squares of the Fibonacci numbers: " + fib_sum);
+        System.out.println("The sum of squares of the Fibonacci numbers: " + fibonachi_sum(fib.getN()));
 
-        fib_sum = 1;
         fib.setN(6);
-        fib.setFib(fibonachi_SQ(6));
+        fib.setFib(fibonachi(6));
 
         System.out.println("The " + fib.getN() + "'s number is: " + fib.getFib());
 
     }
 
-    static int fibonachi_SQ(int N){
+    /**
+     *Generates a Fibonacci number
+     */
+    static int fibonachi(int N){
         int fibonachi = 1;
+        if(N == 1){
+            return 1;
+        }
+        int a = 1, b = 1;
+        for (int i = 2; i < N; i++) {
+            fibonachi = a + b;
+            a = b;
+            b = fibonachi;
+        }
+        return fibonachi;
+    }
+
+    /**
+     *Generates the sum of the squares of the Fibonacci numbers
+     */
+    static int fibonachi_sum(int N){
+        int fib_sum = 1;
+        int fibonachi;
         if(N == 1){
             return 1;
         }
@@ -33,6 +52,6 @@ class Main {
             b = fibonachi;
             fib_sum += fibonachi * fibonachi;
         }
-        return fibonachi;
+        return fib_sum;
     }
 }
