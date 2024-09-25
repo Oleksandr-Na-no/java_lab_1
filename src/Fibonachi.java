37660fab9b62e4c501b66678399c61f4f8ef10e0
Admin <olexandr.11.f@gmail.com>
2024-09-25 11:07:46 +0300

    Changes after review

--- src/Fibonachi.java
+++ src/Fibonachi.java
@@ -2,24 +2,10 @@ public class Fibonachi {
 
     private int N;
     private int fib;
-    private int fib_sum = 1;
 
-    Fibonachi(int N){
+    Fibonachi(int N, int fib) {
         this.N = N;
-        fibonachi_SQ();
-    }
-
-    void fibonachi_SQ(){
-        if(N == 1){
-            fib  = 1;
-        }
-        int a = 1, b = 1;
-        for (int i = 2; i < N; i++) {
-            fib = a + b;
-            a = b;
-            b = fib;
-            fib_sum += fib * fib;
-        }
+        this.fib = fib;
     }
 
     public int getN() {
@@ -28,14 +14,13 @@ public class Fibonachi {
 
     public void setN(int n) {
         N = n;
-        fibonachi_SQ();
     }
 
     public int getFib() {
         return fib;
     }
-
-    public int getFib_sum() {
-        return fib_sum;
+    public void setFib(int fib) {
+        this.fib = fib;
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,38 @@
 import java.util.Scanner;
 
 class Main {
+    static int fib_sum = 1;
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.printf("Enter N: ");
+        System.out.print("Enter N: ");
         int N = sc.nextInt();
 
-        Fibonachi fib = new Fibonachi(N);
+        Fibonachi fib = new Fibonachi(N, fibonachi_SQ(N));
 
-        System.out.println("The sum of squares of the Fibonacci numbers: " + fib.getFib_sum());
+        System.out.println("The " + fib.getN() + "'s number is: " + fib.getFib());
+        System.out.println("The sum of squares of the Fibonacci numbers: " + fib_sum);
 
+        fib_sum = 1;
         fib.setN(6);
-        System.out.println("The sum of squares of number 6 is : " + fib.getFib_sum());
+        fib.setFib(fibonachi_SQ(6));
 
         System.out.println("The " + fib.getN() + "'s number is: " + fib.getFib());
 
+    }
+
+    static int fibonachi_SQ(int N){
+        int fibonachi = 1;
+        if(N == 1){
+            return 1;
+        }
+        int a = 1, b = 1;
+        for (int i = 2; i < N; i++) {
+            fibonachi = a + b;
+            a = b;
+            b = fibonachi;
+            fib_sum += fibonachi * fibonachi;
+        }
+        return fibonachi;
     }
 }
